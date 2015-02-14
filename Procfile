82c5badc77a9a28021cfacf7244d19263aace9d7
Brad Van Vugt <bvanvugt@gmail.com>
2015-02-14 12:48:26 -0800

    Base client.

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn settings.wsgi:application
---
+++ app/main.py
@@ -0,0 +1,44 @@
+import bottle
+import json
+
+
+@bottle.get('/')
+def index():
+    return """
+        <a href="https://github.com/sendwithus/battlesnake-python">
+            battlesnake-python
+        </a>
+    """
+
+
+@bottle.post('/start')
+def start():
+    data = bottle.request.json
+
+    return json.dumps({
+        'name': 'battlesnake-python',
+        'color': '#00ff00',
+        'head_url': 'http://battlesnake-python.herokuapp.com',
+        'taunt': 'battlesnake-python!'
+    })
+
+
+@bottle.post('/move')
+def move():
+    data = bottle.request.json
+
+    return json.dumps({
+        'move': 'left',
+        'taunt': 'battlesnake-python!'
+    })
+
+
+@bottle.post('/end')
+def end():
+    data = bottle.request.json
+
+    return json.dumps({})
+
+
+# Expose WSGI app
+application = bottle.default_app()
