7a189c8ed850bbb5f328e5255d9a6b18734508bb
abraham-delacruz <abraham.delacruz@workday.com>
2020-03-06 14:48:36 -0800

    3pm push

--- app/server.py
+++ app/server.py
@@ -1,7 +1,7 @@
 import json
 import os
 import random
-
+import math
 import bottle
 from bottle import HTTPResponse
 
@@ -28,7 +28,7 @@ def start():
     data = bottle.request.json
     print("START:", json.dumps(data))
 
-    response = {"color": "#00FF00", "headType": "regular", "tailType": "regular"}
+    response = {"color": "#ee0606", "headType": "fang", "tailType": "hook"}
     return HTTPResponse(
         status=200,
         headers={"Content-Type": "application/json"},
@@ -46,63 +46,179 @@ def move():
     data = bottle.request.json
     print("MOVE:", json.dumps(data))
     resp_dict = json.loads(json.dumps(data))
+    distanceTonearestFood = 1000
+    myHealth = resp_dict['you']['health']
     maxHeight = resp_dict['board']['height']
     snakeHeadX = resp_dict['you']['body'][0]['x']
     snakeHeadY = resp_dict['you']['body'][0]['y']
     snakeHeadXY = snakeHeadX, snakeHeadY
+    currentNearestFood = nearestFood(snakeHeadX, snakeHeadY, getFoodSpaces(resp_dict))
+    distanceTonearestFood = calculateDistance(snakeHeadX, currentNearestFood[0],snakeHeadY, currentNearestFood[1])
     # Choose a random direction to move in
     directions = ["up", "down", "left", "right"]
     shout = "I am spaghetti!"
-    #move = random.choice(directions)
-    #conditionally move in a certain direction
-    print("My Spaces: ", getMySpaces(resp_dict))
+    print("My Health: ", myHealth)
+    print("Snake - Head Coordinates: ", snakeHeadXY)
     print("All Snake Spaces: ", getAllSnakeSpaces(resp_dict))
     print("All Food Spaces: ", getFoodSpaces(resp_dict))
-    print("Snakehead Coordinates: ", snakeHeadXY)
-    print("is upSpace Free: ",isUpFree(snakeHeadX, snakeHeadY, getAllSnakeSpaces(resp_dict)))
-    print("is leftSpace Free: ", isLeftFree(snakeHeadX, snakeHeadY, getAllSnakeSpaces(resp_dict)))
-    print("is rightSpace Free: ", isRightFree(snakeHeadX, snakeHeadY, maxHeight, getAllSnakeSpaces(resp_dict)))
-    print("is downSpace Free: ", isDownFree(snakeHeadX, snakeHeadY, maxHeight, getAllSnakeSpaces(resp_dict)))
+    print("Nearest Food Space: ", currentNearestFood, " with a Distance: ", distanceTonearestFood)
+    print("isUpFree: ", isUpFree(snakeHeadX, snakeHeadY, getAllSnakeSpaces(resp_dict)))
+    print("isLeftFree: ", isLeftFree(snakeHeadX, snakeHeadY, getAllSnakeSpaces(resp_dict)))
+    print("isDownFree: ", isDownFree(snakeHeadX, snakeHeadY, maxHeight, getAllSnakeSpaces(resp_dict)))
+    print("isRightFree: ", isRightFree(snakeHeadX, snakeHeadY, maxHeight, getAllSnakeSpaces(resp_dict)))
+    isCloserToFood_RIGHT = isCloserToFood(snakeHeadX + 1, snakeHeadY, currentNearestFood, distanceTonearestFood ) 
+    isCloserToFood_LEFT =  isCloserToFood(snakeHeadX - 1,  snakeHeadY, currentNearestFood, distanceTonearestFood)
+    isCloserToFood_UP =  isCloserToFood(snakeHeadX, snakeHeadY-1, currentNearestFood, distanceTonearestFood)
+    isCloserToFood_DOWN =  isCloserToFood(snakeHeadX, snakeHeadY+1, currentNearestFood, distanceTonearestFood)
+    print("LEFTFOOD:", isCloserToFood_LEFT,"UPFOOD:", isCloserToFood_UP,"DOWNFOOD:", isCloserToFood_DOWN,"RIGHTFOOD:", isCloserToFood_RIGHT,)
+    print("isFoodLeft: ", isFoodLeft(snakeHeadXY,currentNearestFood))
+    print("isFoodRight: ", isFoodRight(snakeHeadXY,currentNearestFood))
+    print("isFoodUp: ", isFoodUp(snakeHeadXY,currentNearestFood))
+    print("isFoodDown: ", isFoodDown(snakeHeadXY,currentNearestFood))
 
-    #Needs some sort of shuffling behavior or the snake just goes in circles until starvation
-    if isUpFree(snakeHeadX, snakeHeadY, getAllSnakeSpaces(resp_dict)):
+    if isFoodUp(snakeHeadXY,getFoodSpaces(resp_dict)):
         response = {"move": "up", "shout": shout}
         return HTTPResponse(
         status=200,
         headers={"Content-Type": "application/json"},
         body=json.dumps(response),
         )
-    elif isLeftFree(snakeHeadX, snakeHeadY, getAllSnakeSpaces(resp_dict)):
+    elif isFoodLeft(snakeHeadXY,getFoodSpaces(resp_dict)):
         response = {"move": "left", "shout": shout}
         return HTTPResponse(
         status=200,
         headers={"Content-Type": "application/json"},
         body=json.dumps(response),
         )
-    elif isDownFree(snakeHeadX, snakeHeadY, maxHeight, getAllSnakeSpaces(resp_dict)):
-        response = {"move": "down", "shout": shout}
-        return HTTPResponse(
-        status=200,
-        headers={"Content-Type": "application/json"},
-        body=json.dumps(response),
-        )
-    elif isRightFree(snakeHeadX, snakeHeadY, maxHeight, getAllSnakeSpaces(resp_dict)):
+    elif isFoodRight(snakeHeadXY,getFoodSpaces(resp_dict)):
         response = {"move": "right", "shout": shout}
         return HTTPResponse(
         status=200,
         headers={"Content-Type": "application/json"},
         body=json.dumps(response),
         )
-    else:   
-        move = random.choice(directions)
-    # Shouts are messages sent to all the other snakes in the game.
-    # Shouts are not displayed on the game board.
-    response = {"move": move, "shout": shout}
-    return HTTPResponse(
+    elif isFoodDown(snakeHeadXY,getFoodSpaces(resp_dict)):
+        response = {"move": "down", "shout": shout}
+        return HTTPResponse(
         status=200,
         headers={"Content-Type": "application/json"},
         body=json.dumps(response),
-    )
+        )
+    elif myHealth >= 80:
+        print("Healthy")
+        if isUpFree(snakeHeadX, snakeHeadY, getAllSnakeSpaces(resp_dict)):
+            print("Healthy UP")
+            response = {"move": "up", "shout": shout}
+            return HTTPResponse(
+            status=200,
+            headers={"Content-Type": "application/json"},
+            body=json.dumps(response),
+            )
+        elif isLeftFree(snakeHeadX, snakeHeadY, getAllSnakeSpaces(resp_dict)):
+            print("Healthy Left")
+            response = {"move": "left", "shout": shout}
+            return HTTPResponse(
+            status=200,
+            headers={"Content-Type": "application/json"},
+            body=json.dumps(response),
+            )
+        elif isDownFree(snakeHeadX, snakeHeadY, maxHeight, getAllSnakeSpaces(resp_dict)):
+            print("Healthy Down")
+            response = {"move": "down", "shout": shout}
+            return HTTPResponse(
+            status=200,
+            headers={"Content-Type": "application/json"},
+            body=json.dumps(response),
+            )
+        elif isRightFree(snakeHeadX, snakeHeadY, maxHeight, getAllSnakeSpaces(resp_dict)):
+            print("Healthy RIGHT")
+            response = {"move": "right", "shout": shout}
+            return HTTPResponse(
+            status=200,
+            headers={"Content-Type": "application/json"},
+            body=json.dumps(response),
+            )
+        else:   
+            move = random.choice(directions)
+        # Shouts are messages sent to all the other snakes in the game.
+        # Shouts are not displayed on the game board.
+        response = {"move": move, "shout": shout}
+        return HTTPResponse(
+            status=200,
+            headers={"Content-Type": "application/json"},
+            body=json.dumps(response),
+        )
+    elif myHealth < 80:
+        print("Hungry")
+        if (isDownFree(snakeHeadX, snakeHeadY, maxHeight, getAllSnakeSpaces(resp_dict)) and isCloserToFood_DOWN):
+            print("Hungry DOWN")           
+            response = {"move": "down", "shout": shout}
+            return HTTPResponse(
+            status=200,
+            headers={"Content-Type": "application/json"},
+            body=json.dumps(response),
+            )
+        elif (isRightFree(snakeHeadX, snakeHeadY, maxHeight, getAllSnakeSpaces(resp_dict)) and isCloserToFood_RIGHT) : 
+            print("Hungry RIGHT")
+            response = {"move": "right", "shout": shout}
+            return HTTPResponse(
+            status=200,
+            headers={"Content-Type": "application/json"},
+            body=json.dumps(response),
+            )
+        elif (isUpFree(snakeHeadX, snakeHeadY, getAllSnakeSpaces(resp_dict)) and isCloserToFood_UP):
+            print("Hungry UP")
+            response = {"move": "up", "shout": shout}
+            return HTTPResponse(
+            status=200,
+            headers={"Content-Type": "application/json"},
+            body=json.dumps(response),
+            )
+        elif (isLeftFree(snakeHeadX, snakeHeadY, getAllSnakeSpaces(resp_dict)) and isCloserToFood_LEFT):
+            print("Hungry LEFT")
+            response = {"move": "left", "shout": shout}
+            return HTTPResponse(
+            status=200,
+            headers={"Content-Type": "application/json"},
+            body=json.dumps(response),
+            )
+        else:
+            move = ""   
+            while move == "":
+                proposedMove = random.choice(directions)
+                if (proposedMove == "up"):
+                    if isUpFree(snakeHeadX, snakeHeadY, getAllSnakeSpaces(resp_dict)):
+                        move = "up"
+                        print("move has been set to up", move)
+                    else:
+                        proposedMove = random.choice(directions) 
+                elif (proposedMove == "left"):
+                    if isLeftFree(snakeHeadX, snakeHeadY, getAllSnakeSpaces(resp_dict)):
+                        move = "left"
+                        print("move has been set to left", move)
+                    else:
+                        proposedMove = random.choice(directions) 
+                elif (proposedMove == "right"):
+                    if isRightFree(snakeHeadX, snakeHeadY, maxHeight, getAllSnakeSpaces(resp_dict)):
+                        move = "right"
+                        print("move has been set to right", move)
+                    else:
+                        proposedMove = random.choice(directions) 
+                elif (proposedMove == "down"):
+                    if isDownFree(snakeHeadX, snakeHeadY, maxHeight, getAllSnakeSpaces(resp_dict)):
+                        move = "down"
+                        print("move has been set to down", move)
+                    else:
+                        proposedMove = random.choice(directions)  
+
+        # Shouts are messages sent to all the other snakes in the game.
+        # Shouts are not displayed on the game board.
+        response = {"move": move, "shout": shout}
+        return HTTPResponse(
+            status=200,
+            headers={"Content-Type": "application/json"},
+            body=json.dumps(response),
+        )
 
 
 @bottle.post("/end")
@@ -129,10 +245,8 @@ def isUpFree(currentX, currentY, totalSnakeSpaces):
     upSpace = currentY - 1
     upCoordinate = currentX, upSpace
     if upSpace == -1: #Top of board Wall
-        print("upspace is a wall")
         return False
     elif upCoordinate in totalSnakeSpaces.values():  #Represents the up Space containing a snake
-        print("upSpace is a snake")
         return False
     return True
 
@@ -140,10 +254,8 @@ def isDownFree(currentX, currentY, maxHeight, totalSnakeSpaces):
     downSpace = currentY + 1
     downCoordinate = currentX, downSpace
     if downSpace == maxHeight: #Bottom of board Wall
-        print("downspace is a wall")
         return False
     elif downCoordinate in totalSnakeSpaces.values():  # Represents the up Space containing a snake
-        print("downspace is a snake")
         return False
     return True
 
@@ -151,10 +263,8 @@ def isLeftFree(currentX, currentY, totalSnakeSpaces):
     leftSpace = currentX - 1
     leftCoordinate = leftSpace, currentY
     if leftSpace == -1: #Left of board Wall
-        print("leftspace is a wall")
         return False
     elif leftCoordinate in totalSnakeSpaces.values():  # BROKEN -  Represents the up Space containing a snake
-        print("leftspace is a snake")
         return False
     return True
 
@@ -162,10 +272,8 @@ def isRightFree(currentX, currentY, maxWidth, totalSnakeSpaces):
     rightSpace = currentX + 1
     rightCoordinate = rightSpace, currentY
     if rightSpace == maxWidth: #Right of board Wall
-        print("rightspace is a wall")
         return False
     elif rightCoordinate in totalSnakeSpaces.values():  # BROKEN - Represents the up Space containing a snake
-        print("rightSpace is a snake")
         return False
     return True
 
@@ -190,6 +298,34 @@ def getAllSnakeSpaces(boardData):
         snake = snake + 1
     return allSnakeSpaces
 
+def isFoodLeft(snakeHead, nearestFood):
+    print("SnakeHeadLeft: ", (snakeHead[0]-1,snakeHead[1]))
+    print((snakeHead[0]-1,snakeHead[1]) in nearestFood)
+    if (snakeHead[0]-1,snakeHead[1]) in nearestFood:
+        return True
+    return False
+def isFoodRight(snakeHead, nearestFood):
+    print("SnakeHeadRight: ", (snakeHead[0]+1,snakeHead[1]))
+    print((snakeHead[0]+1,snakeHead[1]) in nearestFood)
+    if (snakeHead[0]+1,snakeHead[1]) in nearestFood:
+        return True
+    return False
+
+def isFoodUp(snakeHead, nearestFood):
+    print("SnakeHeadUp: ", (snakeHead[0], snakeHead[1]-1))
+    print((snakeHead[0], snakeHead[1]-1) in nearestFood)
+    if (snakeHead[0], snakeHead[1]-1) in nearestFood:
+        return True
+    return False
+
+def isFoodDown(snakeHead, nearestFood):
+    print("SnakeHeadDown: ", (snakeHead[0], snakeHead[1]+1))
+    print((snakeHead[0], snakeHead[1]+1) in nearestFood)
+    if (snakeHead[0], snakeHead[1]+1) in nearestFood:
+        return True
+    return False
+    
+
 def getFoodSpaces(boardData):
     allFoodSpaces = {}
     for food in range(len(boardData['board']['food'])):
@@ -198,6 +334,28 @@ def getFoodSpaces(boardData):
         allFoodSpaces[food] = currentX, currentY
     return allFoodSpaces
 
+def calculateDistance(x1,y1,x2,y2):  
+     dist = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)  
+     return dist  
+
+def nearestFood(currentX, currentY, foodSpaces):
+    nearestFoodCoord = (0,0)
+    shortestDistance = 1000
+    currentSnakeHead = currentX, currentY
+    for foodSpace in range(len(foodSpaces)):
+        thisDistance = calculateDistance(currentX,foodSpaces[foodSpace][0],currentY, foodSpaces[foodSpace][1])
+        if thisDistance < shortestDistance:
+            shortestDistance = thisDistance
+            nearestFoodCoord = foodSpaces[foodSpace][0], foodSpaces[foodSpace][1]
+        foodSpace = foodSpace + 1
+    return nearestFoodCoord
+
+def isCloserToFood(currentX, currentY, closestFood, distance):
+    currentDistance = distance
+    nextDistance = calculateDistance(currentX, closestFood[0], currentY, closestFood[1])
+    if nextDistance < currentDistance:
+        return True
+    return False
 # Expose WSGI app (so gunicorn can find it)
 application = bottle.default_app()
 
