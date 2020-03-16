21b5e2a1aa177c03d7ad9ef1f2f5ad7765d98007
Brad Van Vugt <1531419+bvanvugt@users.noreply.github.com>
2020-03-16 13:27:12 -0700

    Bring back random moves as default.

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 import os
+import random
 
 import cherrypy
 
@@ -26,8 +27,8 @@ class Battlesnake(object):
         # This function is called everytime your snake is entered into a game.
         # cherrypy.request.json contains information about the game that's about to be played.
         # TODO: Use this function to decide how your snake is going to look on the board.
-        print("START")
         data = cherrypy.request.json
+        print("START")
         return {"color": "#888888", "headType": "regular", "tailType": "regular"}
 
     @cherrypy.expose
@@ -37,17 +38,22 @@ class Battlesnake(object):
         # This function is called on every turn of a game. It's how your snake decides where to move.
         # Valid moves are "up", "down", "left", or "right".
         # TODO: Use the information in cherrypy.request.json to decide your next move.
-        print("MOVE")
         data = cherrypy.request.json
-        return {"move": "up"}
+
+        # Choose a random direction to move in
+        possible_moves = ["up", "down", "left", "right"]
+        move = random.choice(possible_moves)
+
+        print(f"MOVE: {move}")
+        return {"move": move}
 
     @cherrypy.expose
     @cherrypy.tools.json_in()
     def end(self):
         # This function is called when a game your snake was in ends.
         # It's purely for informational purposes, you don't have to make any decisions here.
-        print("END")
         data = cherrypy.request.json
+        print("END")
         return "ok"
 
 
@@ -57,4 +63,5 @@ if __name__ == "__main__":
     cherrypy.config.update(
         {"server.socket_port": int(os.environ.get("PORT", "8080")),}
     )
+    print("Starting Battlesnake Server...")
     cherrypy.quickstart(server)
