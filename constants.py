e984713b572b2cb34ec886190bafe853bd1f3cdf
md-hexdrive <57779180+md-hexdrive@users.noreply.github.com>
2020-08-25 10:05:22 -0400

    Add more constants

--- constants.py
+++ constants.py
@@ -4,3 +4,14 @@ SAFE_SPACE = 2
 HAZARD = 3
 
 
+ENEMY_HEAD = 7
+ENEMY_TAIL = 8
+ENEMY_BODY = 9
+
+LEFT = "left"
+RIGHT = "right"
+UP = "up"
+DOWN = "down"
+
+def all_moves():
+    return {"up", "down", "left", "right"}
