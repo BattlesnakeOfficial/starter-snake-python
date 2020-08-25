218e04672bd13f90d3d72482342122b9e0280614
md-hexdrive <57779180+md-hexdrive@users.noreply.github.com>
2020-08-25 10:05:03 -0400

    Add Snake class to represent each battlesnake on the board

---
+++ snake.py
@@ -0,0 +1,35 @@
+
+from util import get_pos
+
+class Snake:
+    def __init__(self, data, is_you=False, is_alive=True):
+        self.snake_id = data['id']
+        self.name = data['name']
+        self.health = data['health']
+        
+        self.head = get_pos(data['head'])#create_point(head, contents=ENEMY_SNAKE_HEAD, parent_object=self)
+        self.body = [self.head]
+        for pos in data['body']:
+            self.body.append(get_pos(pos))
+        self.tail = get_pos(data['body'][-1])
+        self.length = data['length']
+        self.shout = data['shout']
+        self.is_you = is_you
+        self.is_alive = is_alive
+        
+        self.move_hist = []
+    
+    def __str__(self):
+        return self.name
+    
+    def __len__(self):
+        return len(body)
+    def __eq__(self, other):
+        if self.snake_id == other.snake_id:
+            return True
+        else:
+            return False
+    def me(self):
+        return self.is_you
+    def alive(self):
+        return self.is_alive
