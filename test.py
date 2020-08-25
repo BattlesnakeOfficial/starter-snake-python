913cd7cd70d7f3d92c153a5c81fb1d39e0c2025b
md-hexdrive <57779180+md-hexdrive@users.noreply.github.com>
2020-08-25 10:59:52 -0400

    Create test.py
    Create a common file to test code

---
+++ test.py
@@ -0,0 +1,15 @@
+import json
+
+from board import Board
+from collision_avoidance import avoid_obstacles
+
+if __name__ == "__main__":
+  with open("example_move.json") as file:
+    data = json.load(file)
+    board = Board(data)
+    print(board.board)
+    print(board.hazards)
+    print(board.snakes)
+    print(board.is_safe(0,0))
+    
+    print(avoid_obstacles(board, 0, 0))
