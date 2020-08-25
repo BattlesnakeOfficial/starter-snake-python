7fdfbe898610a04a1c3832b02a07322cb9a512d3
md-hexdrive <57779180+md-hexdrive@users.noreply.github.com>
2020-08-25 11:03:17 -0400

    Create collision_avoidance.py
    Add collision avoidance behaviour code

---
+++ collision_avoidance.py
@@ -0,0 +1,28 @@
+import random
+
+import board
+import constants
+import util
+import moves
+
+
+def avoid_obstacles(board, x, y=None):
+    x, y = util.get_pos(x, y)
+    possible_moves = safe_moves(board, x, y)
+    
+    move = random.choice(list(possible_moves.keys()))
+    
+    return move
+
+def safe_moves(board, x, y=None, ignored=[]):
+    x, y = util.get_pos(x, y)
+    possible_moves = moves.get_moves(x, y)
+    returned_moves = dict()
+    
+    for name, move in possible_moves.items():
+        if board.is_safe(move, ignored=ignored):
+            returned_moves[name] = move
+        else:
+            continue
+    return returned_moves
+
