4a1156a7569369269ae39ccafe4b3dcc0c98b911
md-hexdrive <57779180+md-hexdrive@users.noreply.github.com>
2020-09-04 17:19:46 -0400

    Document functions and add failsafes

--- moves.py
+++ moves.py
@@ -5,18 +5,28 @@ class Move:
     def __init__(self, x, y=None):
         self.x, self.y = util.get_pos(x, y)
 
+# return all move names
 def all_moves():
     return ["up", "down", "left", "right"]
 
-
+# get all reasonable {"name": value} dictionary pairs for moves: {"move name": position}
 def get_moves(x, y=None):
     x, y = util.get_pos(x, y)
     moves = dict()
     
-    moves[up()] = move_up(x, y)
-    moves[down()] = move_down(x, y)
-    moves[left()] = move_left(x, y)
-    moves[right()] = move_right(x, y)
+    up_move = move_up(x, y)
+    down_move = move_down(x, y)
+    left_move = move_left(x, y)
+    right_move = move_right(x, y)
+    
+    if up_move >= 0:
+        moves[up()] = up_move
+    if down_move >= 0:
+        moves[down()] = down_move
+    if left_move >= 0:
+        moves[left()] = left_move
+    if right_move >= 0:
+        moves[right()] = right_move
        
     return moves
 
@@ -39,8 +49,12 @@ def move_left(x, y):
 def move_right(x, y):
     return x+1, y
 
+
 def pick_move(possible_moves):
-    return random.choice(list(possible_moves.keys()))
+    if type(possible_moves) == type(dict()):
+        return random.choice(list(possible_moves.keys()))
+    else:
+        return random.choice(list(possible_moves))
 
 if __name__ == "__main__":
     print(get_moves({"x": 0, "y": 0}))
